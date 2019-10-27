03a3cf0f9dd315f0dc6467d48d0dfdc97879d730
Matan Shaviro <matan3sh@gmail.com>
2019-10-27 08:09:58 +0200

    try to add delete booking endpoint

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -24,6 +24,39 @@ router.get('/:id', function (req, res) {
         });
 });
 
+router.delete('/:id', UserCtrl.authMiddleware, function (req, res) {
+    const user = res.locals.user;
+
+    Rental.findById(req.params.id)
+        .populate('user', '_id')
+        .populate({
+            path: 'bookings',
+            select: 'startAt',
+            match: { startAt: { $gt: new Date() } }
+        })
+        .exec(function (err, foundRental) {
+            if (err) {
+                return res.status(422).send({ errors: normalizeErrors(err.errors) });
+            }
+
+            if (user.id !== foundRental.user.id) {
+                return res.status(422).send({ errors: [{ title: 'Invalid User!', detail: 'You are not rental owner!' }] });
+            }
+
+            if (foundRental.bookings.length > 0) {
+                return res.status(422).send({ errors: [{ title: 'Active Bookings', detail: 'Cannot delete rental with active bookings!' }] });
+            }
+
+            foundRental.remove(function (err) {
+                if (err) {
+                    return res.status(422).send({ errors: normalizeErrors(err.errors) });
+                }
+
+                return res.json({'status': 'deleted'});
+            });
+        })
+});
+
 router.post('', UserCtrl.authMiddleware, function(req, res) {
     const { title, city, street, category, image, shared, bedrooms, description, dailyRate } = req.body;
     const user = res.locals.user;
