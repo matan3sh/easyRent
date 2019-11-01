98d207e504847bc767ac95bd0d1f4fbd93546bda
Matan Shaviro <matan3sh@gmail.com>
2019-11-01 18:44:26 +0200

    Adds rental and booking manage section. Adds delete functionality. Displays username on rental detail and disable bookings for no logged in users

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,10 @@ import {
     FETCH_RENTALS_FAIL,
     LOGIN_SUCCESS,
     LOGIN_FAILURE,
-    LOGOUT
+    LOGOUT,
+    FETCH_USER_BOOKINGS_SUCCESS,
+    FETCH_USER_BOOKINGS_FAIL,
+    FETCH_USER_BOOKINGS_INIT
 } from './types';
 
 // RENTALS ACTIONS ------------------------------
@@ -79,6 +82,54 @@ export const createRental = (rentalData) => {
         res => res.data,
         err => Promise.reject(err.response.data.errors)
     )
+}
+
+// USER BOOKINGS ACTIONS ------------------------------
+
+const fetchUserBookingsInit = () => {
+    return {
+        type: FETCH_USER_BOOKINGS_INIT
+    }
+}
+
+const fetchUserBookingsSuccess = (userBookings) => {
+    return {
+        type: FETCH_USER_BOOKINGS_SUCCESS,
+        userBookings
+    }
+}
+
+const fetchUserBookingsFail = (errors) => {
+    return {
+        type: FETCH_USER_BOOKINGS_FAIL,
+        errors
+    }
+}
+
+export const fetchUserBookings = () => {
+    return dispatch => {
+        dispatch(fetchUserBookingsInit());
+
+        axiosInstance.get('/bookings/manage')
+            .then(res => res.data)
+            .then(userBookings => dispatch(fetchUserBookingsSuccess(userBookings)))
+            .catch(({ response }) => dispatch(fetchUserBookingsFail(response.data.errors)));
+    }
+}
+
+// USER RENTALS ACTIONS ------------------------------
+
+export const getUserRentals = () => {
+    return axiosInstance.get('/rentals/manage').then(
+        res => res.data,
+        err => Promise.reject(err.response.data.errors)
+    )
+}
+
+export const deleteRental = (rentalId) => {
+    return axiosInstance.delete(`/rentals/${rentalId}`).then(
+        res => res.data,
+        err => Promise.reject(err.response.data.errors))
 }
 
 // AUTH ACTIONS ------------------------------
--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -3,11 +3,13 @@ import DateRangePicker from 'react-bootstrap-daterangepicker';
 import { ToastContainer, toast } from 'react-toastify';
 import { BookingModal } from './BookingModal';
 import { getRangeOfDates } from 'helpers';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import * as moment from 'moment';
 import * as actions from 'actions';
 
-export class Booking extends React.Component {
+class Booking extends React.Component {
 
   constructor() {
     super();
@@ -92,12 +94,12 @@ export class Booking extends React.Component {
   resetData() {
     this.dateRef.current.value = '';
 
-    this.setState({proposedBooking: {guests: ''}});
+    this.setState({ proposedBooking: { guests: '' } });
   }
 
   confirmProposedData() {
-    const {startAt, endAt} = this.state.proposedBooking;
-    const days = getRangeOfDates(startAt, endAt).length -1;
+    const { startAt, endAt } = this.state.proposedBooking;
+    const days = getRangeOfDates(startAt, endAt).length - 1;
     const { rental } = this.props;
 
     this.setState({
@@ -121,14 +123,14 @@ export class Booking extends React.Component {
         this.resetData();
         toast.success('Booking has been succesfuly created! Enjoy :)')
       },
-      
+
       (errors) => {
-        this.setState({errors});
+        this.setState({ errors });
       })
   }
 
   render() {
-    const { rental } = this.props;
+    const { rental, auth: { isAuth } } = this.props;
     const { startAt, endAt, guests } = this.state.proposedBooking;
 
     return (
@@ -136,39 +138,56 @@ export class Booking extends React.Component {
         <ToastContainer />
         <h3 className='booking-price'>${rental.dailyRate} <span className='booking-per-night'>per night</span></h3>
         <hr></hr>
-        <div className='form-group'>
-          <label htmlFor='dates'>Dates</label>
-          <DateRangePicker onApply={this.handleApply}
-            isInvalidDate={this.checkInvalidDates}
-            opens='left'
-            containerStyles={{ display: 'block' }}>
-            <input ref={this.dateRef} id='dates' type='text' className='form-control'></input>
-          </DateRangePicker>
-        </div>
-        <div className='form-group'>
-          <label htmlFor='guests'>Guests</label>
-          <input onChange={(event) => { this.selectGuests(event) }}
-            value={guests}
-            type='number'
-            className='form-control'
-            id='guests'
-            aria-describedby='guests'
-            placeholder=''>
-          </input>
-        </div>
-        <button disabled={!startAt || !endAt || !guests} onClick={() => this.confirmProposedData()} className='btn btn-bwm btn-confirm btn-block'>Reserve place now</button>
+        { !isAuth &&
+            <Link className='btn btn-bwm btn-confirm btn-block' to={{pathname: '/login'}}>
+              Login to book place.
+            </Link>
+        }
+        {isAuth &&
+          <React.Fragment>
+            <div className='form-group'>
+              <label htmlFor='dates'>Dates</label>
+              <DateRangePicker onApply={this.handleApply}
+                isInvalidDate={this.checkInvalidDates}
+                opens='left'
+                containerStyles={{ display: 'block' }}>
+                <input ref={this.dateRef} id='dates' type='text' className='form-control'></input>
+              </DateRangePicker>
+            </div>
+            <div className='form-group'>
+              <label htmlFor='guests'>Guests</label>
+              <input onChange={(event) => { this.selectGuests(event) }}
+                value={guests}
+                type='number'
+                className='form-control'
+                id='guests'
+                aria-describedby='guests'
+                placeholder=''>
+              </input>
+            </div>
+            <button disabled={!startAt || !endAt || !guests} onClick={() => this.confirmProposedData()} className='btn btn-bwm btn-confirm btn-block'>Reserve place now</button>
+          </React.Fragment>
+        }
         <hr></hr>
         <p className='booking-note-title'>People are interested into this house</p>
         <p className='booking-note-text'>
           More than 500 people checked this rental in last month.
         </p>
-        <BookingModal open={this.state.modal.open} 
-                      closeModal={this.cancelConfirmation}
-                      confirmModal={this.reserveRental} 
-                      booking={this.state.proposedBooking}
-                      errors={this.state.errors} 
-                      rentalPrice={rental.dailyRate} />
+        <BookingModal open={this.state.modal.open}
+          closeModal={this.cancelConfirmation}
+          confirmModal={this.reserveRental}
+          booking={this.state.proposedBooking}
+          errors={this.state.errors}
+          rentalPrice={rental.dailyRate} />
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(Booking);
